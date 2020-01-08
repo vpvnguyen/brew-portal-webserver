64e755d55bd6732772ef4413ab5ca498fda45258
vpvnguyen <vpvnguyen@gmail.com>
2020-01-07 21:45:21 -0800

    auth routing

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-// const jwt = require('express-jwt'); 
-const querystring = require('querystring');
 const keys = require('../config/env_config'); 
 const User = require('../db/user'); 
 
-const CLIENT_HOME_PAGE_URL = 'http://localhost:3000'
+const CLIENT_HOME_PAGE_URL = 'http://localhost:3000';
 
 // when login is successful, retrieve user info
 router.get("/login/success/:id", async (req, res) => {
@@ -20,37 +18,12 @@ router.get("/login/success/:id", async (req, res) => {
     })
 });
 
-// router.get('/login', function(req, res, next) {
-//   passport.authenticate('google', function(err, user, info) {
-//     if (err) return err; 
-//     if (user) {
-//       jwtConverter.sign(user, keys.JWTSECRET,  function(err, token) {
-//         // console.log('hit: ' + JSON.parse(token))
-//         res.json({
-//           success: true,
-//           message: "user has successfully authenticated",
-//           user: user, 
-//           token: token
-//         });
-//       });
-//     }
-//   })(req, res, next);
-// });
-
 //auth log out 
 router.get('/logout', (req, res) => {
     //handle with passport 
     req.logout(); 
     res.redirect(CLIENT_HOME_PAGE_URL); 
 }); 
-
-//failre redirect 
-// router.get('/login/failed', (req, res) => {
-//     res.status(401).json({
-//         success: false, 
-//         message: 'user failed to authenticate'
-//     }); 
-// }); 
 
 //google auth 
 router.get('/google', passport.authenticate('google', {
@@ -59,14 +32,6 @@ router.get('/google', passport.authenticate('google', {
     failureRedirect: CLIENT_HOME_PAGE_URL
   })
 );
-
-// google auth redirect 
-// router.get('/google/redirect', passport.authenticate('google', {
-//         session: false,
-//         successRedirect: 'http://localhost:3000/splash/' + req.user.id, 
-//         failureRedirect: '/auth/login/failed', 
-//     })
-// ); 
 
 // //google auth redirect 
 router.get('/google/redirect', passport.authenticate('google', { session: false }), (req, res) => {
@@ -79,10 +44,8 @@ router.get('/google/redirect', passport.authenticate('google', { session: false 
   }
 }); 
 
-
 router.get('/dashboard', (req, res) => {
   res.send()
 }); 
-
 
 module.exports = router;
