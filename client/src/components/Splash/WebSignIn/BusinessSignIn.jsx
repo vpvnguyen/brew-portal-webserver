72af5a216a6d949be13159d1ce7da003d967839a
vpvnguyen <vpvnguyen@gmail.com>
2020-01-07 21:42:54 -0800

    business sign in and claim a new/another business

--- client/src/components/Splash/WebSignIn/BusinessSignIn.jsx
+++ client/src/components/Splash/WebSignIn/BusinessSignIn.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
-
 // material-ui
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-
-// components
-import Dashboard from '../../BusinessPortal/Dashboard/Dashboard.js';
 
 // styling
 const useStyles = makeStyles(theme => ({
