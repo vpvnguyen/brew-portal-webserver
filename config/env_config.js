2639de49ce3ab44e89bbaa4aa416dfa1ef897398
vpvnguyen <vpvnguyen@gmail.com>
2020-01-07 21:43:45 -0800

    configure to use dotenv for keys and tokens

--- config/env_config.js
+++ config/env_config.js
@@ -1,15 +1,13 @@
-//requires dotenv file 
+// requires dotenv file 
 const dotenv = require('dotenv');
 dotenv.config();
 
-//exports passwords aliased in camel case
+// exports passwords aliased in camel case
 module.exports = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
   dbPassword: process.env.DB_PASSWORD,
-
   privateKey: process.env.PRIVATE_KEY,
   YELP_CLIENT: process.env.YELP_CLIENT,
-
 };
